191a10fd1ef7489ef28fc4807e62c86c110527fc
heraclex <toan.le.group@gmail.com>
2013-06-13 17:10:07 -0700

    trying implement waiting process

--- Cik.MagazineWeb.WebApp/App/durandal/http.js
+++ Cik.MagazineWeb.WebApp/App/durandal/http.js
@@ -2,7 +2,34 @@
     return {
         defaultJSONPCallbackParam:'callback',
         get:function(url, query) {
-            return $.ajax(url, { data: query });
+            // return $.ajax(url, { data: query }); 
+            //style='display: none'
+
+            var style = "style='display: block; margin: auto; backgroundColor: transparent; border: none; zIndex: 10002'";
+            var imageLoading = "<img id='ImageProcessing' src='/Content/images/InProgress.gif' alt='Loading...' " + style + " />";
+            
+            // var styleDivLoading = "style = 'height: 400px; position: relative; background-color: gray; /* for demonstration */";
+            // var divLoading = "<div id='LoadingImage' >" + imageLoading + "</div>";
+            $('body').append(imageLoading);
+            //$.blockUI();
+            
+            // show loading
+            return $.ajax({
+                url: url,
+                data: query,
+                type: 'GET',
+                contentType: 'application/json',
+                dataType: 'json',
+                success: function() { 
+                    //$.unblockUI();
+                    $("#ImageProcessing").remove();
+                }, 
+                error: function (xhr, status) {
+                    //$.unblockUI();
+                    $("#ImageProcessing").remove();
+                } 
+            });
+            
         },
         jsonp: function (url, query, callbackParam) {
             if (url.indexOf('=?') == -1) {
